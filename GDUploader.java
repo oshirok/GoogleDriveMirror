73022ad324f16df35ad6ec72632ea1c2ce9e595d
noodledevelopmentco <noodledevelopmentco@gmail.com>
2015-06-27 16:12:25 -0700

    Added documentation

--- GDUploader.java
+++ GDUploader.java
@@ -12,30 +12,32 @@ import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 import com.google.api.services.drive.model.ParentReference;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 
-public class GDTest {
+public class GDUploader {
 
 	private static String CLIENT_ID = "573610413961-6a6imko3r9h83laqo4i1i9c50q878hja.apps.googleusercontent.com";
 	private static String CLIENT_SECRET = "nosQ6RfgUAnHchsyAOemolkJ";
-
 	private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 
 	public static void main(String[] args) throws IOException {
 		HttpTransport httpTransport = new NetHttpTransport();
 		JsonFactory jsonFactory = new JacksonFactory();
 
+		// For authorizing with Google Account
 		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
 				httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
 		.setAccessType("online")
@@ -44,32 +46,57 @@ public class GDTest {
 		String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
 		System.out.println("Please open the following URL in your browser then type the authorization code:");
 		System.out.println("  " + url);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String code = br.readLine();
+		Scanner scanner = new Scanner(System.in);
+		String code = scanner.nextLine();
 
 		GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
 		GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
 
 		//Create a new authorized API client
 		Drive service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
-		Set<String> tmp1 = listLocalFiles();
-		Set<String> tmp2 = retrieveAllGDFiles(service);
+		
+		// Ask the user for the local folder source name
+		System.out.println("Enter: the local source folder name:");
+		String local_folder_name = scanner.nextLine();
+		
+		// Verify that the file exists, if not prompt again
+		java.io.File f = new java.io.File(local_folder_name);
+		while(!f.exists() || !f.isDirectory()) {
+			System.out.println("Invalid File. Enter: the local source folder name:");
+			local_folder_name = scanner.nextLine();
+		}
+		
+		// Now Ask for and verify that the Google Drive target folder exists
+		String folder_id = null;
+		boolean folder_found = false;
+		while(!folder_found) {
+			System.out.println("Enter: the Google Drive target folder name:");
+			String name = scanner.nextLine();
+			try {
+				folder_id = getFolderByName(name, service);
+				folder_found = true;
+			} catch (IOException e) {
+				System.out.println("Google Drive target name not found try again");
+			} catch (IllegalArgumentException e) {
+				System.out.println("Google Drive target name not found try again");
+			}
+			// Prompt for folder name
+		}
+		
+		// Set operations to get files that dont already exist
+		Set<String> tmp1 = listLocalFiles(local_folder_name);
+		Set<String> tmp2 = retrieveAllGDFiles(service, folder_id);
 		tmp2.retainAll(tmp1);
 		tmp1.removeAll(tmp2);
+		
+		// Split the files array recursively for Parallel Processing
 		String[] files = tmp1.toArray(new String[tmp1.size()]);
 		String[] files1 = new String[files.length / 2];
 		String[] files2 = new String[files.length - files.length / 2];
 		System.arraycopy(files, 0, files1, 0, files1.length);
 		System.arraycopy(files, files1.length, files2, 0, files2.length);
 		
-		Upload.uploadArray(files, service);
-		/*java.util.Iterator<String> itty = tmp1.iterator();
-		while (itty.hasNext()) {
-			String filename = itty.next();
-			File file = insertFile(service, filename, "",
-					"0B5c7iu0bimJXfkZZVlR2eHFQd0paY2lHYWVQM3FfeWJ0WjYya1NkMlJtWjJER05OWVd1UkE", java.nio.file.Files.probeContentType(Paths.get("150D5200/" + filename)), "150D5200/" + filename);
-			System.out.println(file.getTitle());
-		}*/
+		Upload.uploadArray(files, service, folder_id, local_folder_name);
 		System.out.println("DONE!");
 	}
 
@@ -79,9 +106,9 @@ public class GDTest {
 	 * @param service Drive API service instance.
 	 * @return List of File resources.
 	 */
-	private static Set<String> retrieveAllGDFiles(Drive service) throws IOException {
+	private static Set<String> retrieveAllGDFiles(Drive service, String folderid) throws IOException {
 		List<File> result = new ArrayList<File>();
-		Files.List request = service.files().list().setQ("'0B5c7iu0bimJXfkZZVlR2eHFQd0paY2lHYWVQM3FfeWJ0WjYya1NkMlJtWjJER05OWVd1UkE' in parents");
+		Files.List request = service.files().list().setQ("'" + folderid + "'" + " in parents");
 		Set<String> setty = new HashSet<String>();
 
 		System.out.println("Loading files from Google Drive...");
@@ -104,8 +131,8 @@ public class GDTest {
 		return setty;
 	}
 
-	private static Set<String> listLocalFiles() {
-		java.io.File folder = new java.io.File("150D5200");
+	private static Set<String> listLocalFiles(String foldername) {
+		java.io.File folder = new java.io.File(foldername);
 		java.io.File[] listOfFiles = folder.listFiles();
 		Set<String> setty = new HashSet<String>();
 
@@ -117,6 +144,35 @@ public class GDTest {
 			}
 		}
 		return setty;
+	}
+	
+	private static Map<String, File> getFolders(Drive service) throws IOException {
+		List<File> result = new ArrayList<File>();
+		Files.List request = service.files().list().setQ("mimeType = 'application/vnd.google-apps.folder'");
+		Map<String, File> setty = new HashMap<String, File>();
+		do {
+			try {
+				FileList files = request.execute();
+				result.addAll(files.getItems());
+				request.setPageToken(files.getNextPageToken());
+			} catch (IOException e) {
+				System.out.println("An error occurred: " + e);
+				request.setPageToken(null);
+			}
+		} while (request.getPageToken() != null &&
+				request.getPageToken().length() > 0);
+
+		for(File file: result) {
+			setty.put(file.getTitle(), file);
+			// System.out.println(file.getTitle());
+		}
+		return setty;
+	}
+	
+	private static String getFolderByName(String name, Drive service) throws IOException {
+		Map<String, File> mappy = getFolders(service);
+		if (mappy.get(name) == null) throw new IllegalArgumentException("Enter a valid folder name");
+		return mappy.get(name).getId();
 	}
 
 	/**
@@ -171,12 +227,16 @@ public class GDTest {
 	    int high;
 	    String[] array;
 	    Drive service;
+	    String folderid;
+	    String foldername;
 	
-	    Upload(String[] arr, int lo, int hi, Drive serv) {
+	    Upload(String[] arr, int lo, int hi, Drive serv, String fo_id, String local_fo_name) {
 	        array = arr;
 	        low   = lo;
 	        high  = hi;
 	        service = serv;
+	        folderid = fo_id;
+	        foldername = local_fo_name;
 	    }
 	
 	    protected Long compute() {
@@ -186,7 +246,7 @@ public class GDTest {
 					File file;
 					try {
 						file = insertFile(service, filename, "",
-								"0B5c7iu0bimJXfkZZVlR2eHFQd0paY2lHYWVQM3FfeWJ0WjYya1NkMlJtWjJER05OWVd1UkE", java.nio.file.Files.probeContentType(Paths.get("150D5200/" + filename)), "150D5200/" + filename);
+								folderid, java.nio.file.Files.probeContentType(Paths.get(foldername + "/" + filename)), foldername + "/" + filename);
 						System.out.println(file.getTitle());
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
@@ -195,8 +255,8 @@ public class GDTest {
 	        	}
 	         } else {
 	            int mid = low + (high - low) / 2;
-	            Upload left  = new Upload(array, low, mid, service);
-	            Upload right = new Upload(array, mid, high, service);
+	            Upload left  = new Upload(array, low, mid, service, folderid, foldername);
+	            Upload right = new Upload(array, mid, high, service, folderid, foldername);
 	            left.fork();
 	            right.compute();
 	            left.join();
@@ -204,8 +264,8 @@ public class GDTest {
             return (long) 0;
 	     }
 	
-	     static void uploadArray(String[] array, Drive serv) {
-	         Globals2.fjPool.invoke(new Upload(array,0,array.length, serv));
+	     static void uploadArray(String[] array, Drive serv, String folderid, String foldername) {
+	         Globals2.fjPool.invoke(new Upload(array,0,array.length, serv, folderid, foldername));
 	     }
 	}
 
